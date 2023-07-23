2c1721f0952b4ac4b3d705f7b7428a3831b334f6
Datisekai <datly030102@gmail.com>
2023-07-23 12:04:42 +0700

    fix: implement cònig

--- src/main.js
+++ src/main.js
@@ -3,19 +3,22 @@ import Phaser from "phaser";
 import Preload from "./scene/Preload";
 import Bootstrap from "./scene/Bootstrap";
 import Play from "./scene/Play";
+import configDefault from "./config";
+
+const { width, height } = configDefault;
 
 const config = {
   type: Phaser.AUTO,
   parent: "app",
-  width: 800,
-  height: 600,
+  width,
+  height,
   physics: {
     default: "arcade",
     arcade: {
       gravity: { y: 200 },
     },
   },
-  scene: [Preload, Bootstrap, Play	],
+  scene: [Preload, Bootstrap, Play],
 };
 
 export default new Phaser.Game(config);
